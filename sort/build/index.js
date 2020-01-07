be0e863f0da597eb61a26c1efffae3650acfb250
mathewvicente <mathewvicente@gmail.com>
2020-01-07 17:14:20 -0300

    Add LinkedList class, needs to fix sort from this collection

--- sort/build/index.js
+++ sort/build/index.js
@@ -3,6 +3,7 @@ Object.defineProperty(exports, "__esModule", { value: true });
 var Sorter_1 = require("./Sorter");
 var NumbersCollection_1 = require("./NumbersCollection");
 var CharactersCollection_1 = require("./CharactersCollection");
+var LinkedList_1 = require("./LinkedList");
 // Sort array of numbers
 var numbersCollection = new NumbersCollection_1.NumbersCollection([10, 3, -5, 0]);
 var sorter = new Sorter_1.Sorter(numbersCollection);
@@ -13,3 +14,12 @@ var charactersCollection = new CharactersCollection_1.CharactersCollection('Xaay
 var sorter2 = new Sorter_1.Sorter(charactersCollection);
 sorter2.sort();
 console.log(sorter2.collection);
+// Sort linked list
+var linkedList = new LinkedList_1.LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+var sorter3 = new Sorter_1.Sorter(linkedList);
+sorter3.sort();
+linkedList.print();
--- sort/src/index.ts
+++ sort/src/index.ts
@@ -1,6 +1,7 @@
 import { Sorter } from './Sorter';
 import { NumbersCollection } from './NumbersCollection';
 import { CharactersCollection } from './CharactersCollection';
+import { LinkedList } from './LinkedList';
 
 // Sort array of numbers
 const numbersCollection = new NumbersCollection([10, 3, -5, 0]);
@@ -13,3 +14,15 @@ const charactersCollection  = new CharactersCollection('Xaayb');
 const sorter2 = new Sorter(charactersCollection);
 sorter2.sort();
 console.log(sorter2.collection);
+
+
+// Sort linked list
+const linkedList = new LinkedList();
+linkedList.add(500);
+linkedList.add(-10);
+linkedList.add(-3);
+linkedList.add(4);
+
+const sorter3 = new Sorter(linkedList);
+sorter3.sort();
+linkedList.print();
