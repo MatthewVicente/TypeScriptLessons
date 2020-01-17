38bcc1f85be4f6d38ec1a07613a0628f59c691bf
mathewvicente <mathewvicente@gmail.com>
2020-01-17 12:07:57 -0300

    Redo Match Reader and CsvFileReader using interfaces

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,13 +1,18 @@
 import { MatchResult } from './MatchResults';
+import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
 
-const reader = new MatchReader("football.csv");
-reader.read();
-const matches = reader.data;
+// Create an object that satisfies the 'DataReader' interface
+const csvFileReader = new CsvFileReader('football.csv');
+
+// Create an instance of MatchReader and pass in something satisfying
+// the 'DataReader' interface
+const matchReader = new MatchReader(csvFileReader);
+matchReader.load();
 
 let manUnitedWins = 0;
 
-for (let match of matches) {
+for (let match of matchReader.matches) {
     if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) {
         manUnitedWins++;
     } else if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) {
