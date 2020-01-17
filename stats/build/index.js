7530e9ec08af9156bfdec9c469162b6a926a3f64
mathewvicente <mathewvicente@gmail.com>
2020-01-17 10:42:14 -0300

    Make CsvFileReader work with any type of data structure

--- stats/build/index.js
+++ stats/build/index.js
@@ -1,8 +1,8 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-var CsvFileReader_1 = require("./CsvFileReader");
 var MatchResults_1 = require("./MatchResults");
-var reader = new CsvFileReader_1.CsvFileReader("football.csv");
+var MatchReader_1 = require("./MatchReader");
+var reader = new MatchReader_1.MatchReader("football.csv");
 reader.read();
 var matches = reader.data;
 var manUnitedWins = 0;
