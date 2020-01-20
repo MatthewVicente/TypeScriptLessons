a6706cc7e2ff0e67137548f0f3bd42120d6cc1aa
mathewvicente <mathewvicente@gmail.com>
2020-01-20 14:11:31 -0300

    Create static methods to improve code

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -1,21 +1,8 @@
-import { CsvFileReader } from './CsvFileReader';
 import { MatchReader } from './MatchReader';
-import { ConsoleReport } from './reportTargets/ConsoleReport';
-import { HtmlReport } from './reportTargets/HtmlReport';
-import { WinsAnalysis } from './analyzers/WinsAnalysis';
 import { Summary } from './Summary';
 
-// Create an object that satisfies the 'DataReader' interface
-const csvFileReader = new CsvFileReader('football.csv');
+const matchReader = MatchReader.fromCsv("football.csv");
+const summary = Summary.winsAnalysisWithHtmlReport('Man United');
 
-// Create an instance of MatchReader and pass in something satisfying
-// the 'DataReader' interface
-const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
-
-const summary = new Summary(
-	new WinsAnalysis('Man United'),
-	new HtmlReport()
-);
-
 summary.buildAndPrintReport(matchReader.matches);
